6fc5b8e711622e11c3de6ab4d26701fcbb919b15
yf728784164 <107985941+yf728784164@users.noreply.github.com>
2026-06-04 11:16:33 +0800

    fix project cover display

--- src/App.jsx
+++ src/App.jsx
@@ -864,7 +864,7 @@ function ProductDesignPage({ onBack }) {
             <img
               src={currentProject.process[index]}
               alt={step}
-              className="h-full w-full object-cover"
+              className="h-full w-full object-contain bg-[#030303]"
             />
           </div>
 
